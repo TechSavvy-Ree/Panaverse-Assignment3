062f074983f6f142b1a4d808e657317a9b10538d
TechSavvy-Ree <ranya7476@gmail.com>
2024-01-13 17:48:10 -1200

    Determines the day of the week by input number

---
+++ Ex-9.ts
@@ -0,0 +1,23 @@
+// Develop a program that determines the day of the week. Ask the user for a number (1-7) and use nested if statements to print the corresponding day's name.
+let inputNumber = 3;
+// let inputNumber = 8; ( it prints else statment " Invalid input")
+if ( inputNumber>= 1 && inputNumber <= 7) {
+    if (inputNumber === 1) {
+        console.log("Day of the week: Sunday");
+    } else if ( inputNumber=== 2) {
+        console.log("Day of the week: Monday");
+    } else if (inputNumber === 3) {
+        console.log("Day of the week: Tuesday");
+    } else if (inputNumber === 4) {
+        console.log("Day of the week: Wednesday");
+    } else if (inputNumber === 5) {
+        console.log("Day of the week: Thursday");
+    } else if (inputNumber === 6) {
+        console.log("Day of the week: Friday");
+    } else {
+        console.log("Day of the week: Saturday");
+    }
+} else {
+    console.log("Invalid input. Please enter a number between 1 and 7.");
+}
+
