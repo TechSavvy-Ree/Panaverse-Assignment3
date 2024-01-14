4ec3a0cbb2989868b649469bf506528184985b5f
TechSavvy-Ree <ranya7476@gmail.com>
2024-01-13 13:16:10 -1200

    Q1: Temperature Conversion

---
+++ Ex-1.ts
@@ -0,0 +1,15 @@
+// Write a program to convert the temperature from Celsius to Fahrenheit and vice verse
+function celsiusToFahrenheit(celsius: number): number {
+    return (celsius * 9 / 5) + 32;
+}
+function fahrenheitToCelsius(fahrenheit: number): number {
+    return (fahrenheit - 32) * 5 / 9;
+}
+ // Example usage:
+const celsiusTemperature = 25;
+const convertedToFahrenheit = celsiusToFahrenheit(celsiusTemperature);
+console.log(`${celsiusTemperature}°C is ${convertedToFahrenheit}°F`);
+
+const fahrenheitTemperature = 77;
+const convertedToCelsius = fahrenheitToCelsius(fahrenheitTemperature);
+console.log(`${fahrenheitTemperature}°F is ${convertedToCelsius}°C`);
