347ae1759627d45964dbf6bf619c8c95c06e0bf9
TechSavvy-Ree <ranya7476@gmail.com>
2024-01-13 17:19:49 -1200

    Check  year is Leap year or not

---
+++ Ex-8.ts
@@ -0,0 +1,10 @@
+// Write a program that checks if the given year is leap year or not.
+let givenYear = 2024;
+
+let isLeapYear = (givenYear % 4 === 0 && givenYear % 100 !== 0) || (givenYear % 400 === 0);
+
+if (isLeapYear) {
+    console.log(`${givenYear} is a leap year.`);
+} else {
+    console.log(`${givenYear} is not a leap year.`);
+}
