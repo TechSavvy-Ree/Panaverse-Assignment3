ea1e4012ff8b9eff914321b30219ad635e019840
TechSavvy-Ree <ranya7476@gmail.com>
2024-01-13 19:29:45 -1200

    Total Bill with Tax

---
+++ Ex-10.js
@@ -0,0 +1,16 @@
+"use strict";
+/*Write a program that takes the number of units consumed by a user if it is greater than 100 then add 10% tax
+if greater than 200 then add 15% of tax so on up to if greater than 500 then add 25% of tax Where the tax amount will be calculated by the amount of bill.*/
+let units = 250;
+let unitPrice = 10; // Assuming Rs.10 per unit
+let totalBill = units * unitPrice;
+if (units > 500) {
+    totalBill = totalBill + (totalBill * 0.25); // Add 25% tax for consumption above 500 units
+}
+else if (units > 200) {
+    totalBill = totalBill + (totalBill * 0.15); // Add 15% tax for consumption above 200 units
+}
+else if (units > 100) {
+    totalBill = totalBill + (totalBill * 0.1); // Add 10% tax for consumption above 100 units
+}
+console.log(`Total Amount: Rs.${totalBill}`);
